6baf13b9e55673c4ab46946feae655db8e2722ee
Sameer <smanandhar1@uclan.ac.uk>
2023-12-21 15:22:20 +0000

    Added cli args

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
+use clap::Parser;
+
 
 pub mod convert_to_struct;
+pub mod console;
+
 fn main() {
+    let args = console::Args::parse();
     convert_to_struct::json_to_struct("test.json", "Map")
 }
